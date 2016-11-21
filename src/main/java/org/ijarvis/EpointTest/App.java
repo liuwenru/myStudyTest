ba3b5124c7ff999865392c890636d9f5f2d80176
liuwenru <ijarvis@sina.com>
2016-11-21 23:43:56 +0800

    init

---
+++ src/main/java/org/ijarvis/EpointTest/App.java
@@ -0,0 +1,21 @@
+package org.ijarvis.EpointTest;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class App extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(req.getParameter("name"));
+	}
+}
