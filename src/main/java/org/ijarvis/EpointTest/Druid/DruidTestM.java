ab3219ffe4b00b9101bc2cc498f47a3af051580c
liuwenru <ijarvis@sina.com>
2016-11-23 11:01:22 +0800

    add druif

---
+++ src/main/java/org/ijarvis/EpointTest/Druid/DruidTestM.java
@@ -0,0 +1,27 @@
+package org.ijarvis.EpointTest.Druid;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Test;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+public class DruidTestM {
+	
+	public static void GetUrlinfo(){
+		
+	}
+	public static void druidInsert() throws BeansException, SQLException{
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		DruidDataSource datasource= (DruidDataSource) context.getBean("dataSource");
+		Connection connection=datasource.getConnection();
+		Statement command=connection.createStatement();
+		command.execute("insert into testinfo(name,pass) values('222','222')");
+	}
+
+}
--- src/test/java/org/ijarvis/EpointTest/JunitlTest/DruidTest.java
+++ src/test/java/org/ijarvis/EpointTest/JunitlTest/DruidTest.java
@@ -1,14 +1,20 @@
 package org.ijarvis.EpointTest.JunitlTest;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
+import org.ijarvis.EpointTest.Druid.DruidTestM;
 import org.ijarvis.EpointTest.DruidMonitor.DruidMonitorTest;
 import org.junit.Test;
+import org.springframework.beans.BeansException;
 
 public class DruidTest {
 	@Test
-	public void DruidTest() throws IOException{
+	public void DruidTest() throws IOException, BeansException, SQLException{
 		DruidMonitorTest.GetDruidMonitorInfo();
+		while(true){
+			DruidTestM.druidInsert();
+		}
 	}
 
 }
