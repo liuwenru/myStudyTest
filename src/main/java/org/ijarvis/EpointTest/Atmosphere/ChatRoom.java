f3d12fb619f5c966346283a23345341db03c077d
liuwenru <ijarvis@sina.com>
2016-12-14 09:45:01 +0800

    websocket ok

--- src/main/java/org/ijarvis/EpointTest/Atmosphere/ChatRoom.java
+++ src/main/java/org/ijarvis/EpointTest/Atmosphere/ChatRoom.java
@@ -2,14 +2,30 @@ package org.ijarvis.EpointTest.Atmosphere;
 import org.apache.log4j.Logger;
 import org.atmosphere.config.service.ManagedService;
 import org.atmosphere.config.service.Message;
+import org.atmosphere.config.service.Ready;
 import org.atmosphere.cpr.AtmosphereResource;
-
-
-@ManagedService(path="/chat")
+import static org.atmosphere.cpr.ApplicationConfig.MAX_INACTIVE;
+@ManagedService(path = "/websocket/chat",atmosphereConfig = MAX_INACTIVE + "=120000")
 public class ChatRoom{
 	private static final Logger logger=Logger.getLogger(ChatRoom.class);
+	@Ready
+	public String onReadys(final AtmosphereResource r) {
+
+		logger.debug("Browser {} connected.");
+		return "bbbbb";
+	}
+	@Ready
+	public String onReady(final AtmosphereResource r) {
+
+		logger.debug("Browser {} connected.");
+		return "aaaaa";
+	}
+
 	@Message
 	public void OnMessage(AtmosphereResource r,String message){
+
 		logger.debug("OnMessage is ready............."+message);
+
+
 	}
 }
