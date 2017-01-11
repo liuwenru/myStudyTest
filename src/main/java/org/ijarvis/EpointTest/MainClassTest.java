d3151c7578569aedef7e7e19e21236cd8d05dda2
liuwenru <ijarvis@sina.com>
2017-01-11 15:50:46 +0800

    init ok

--- src/main/java/org/ijarvis/EpointTest/MainClassTest.java
+++ src/main/java/org/ijarvis/EpointTest/MainClassTest.java
@@ -7,10 +7,6 @@ import org.bson.Document;
 
 public class MainClassTest {
 	public static void main(String[] args) throws UnknownHostException {
-		
-		
-		
-		
-		
+		System.out.println(System.getProperties().getProperty("os.name"));
 	}
 }
