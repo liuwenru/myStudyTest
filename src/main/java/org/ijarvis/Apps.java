4fffaaf6b3442dbfabe33f61cd9c7637605a65d9
liuwenru <ijarvis@sina.com>
2017-02-21 09:26:26 +0800

    add all file

---
+++ src/main/java/org/ijarvis/Apps.java
@@ -0,0 +1,30 @@
+package org.ijarvis;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liuwenru on 2017/2/20.
+ */
+@Controller
+@RequestMapping("/index")
+public class Apps {
+
+    static  class  OOMObject{
+        public  byte[] plac=new byte[64*1024];
+    }
+    @RequestMapping("/home")
+    public String hello(@RequestParam(name = "lenth") int num) throws InterruptedException {
+        List<OOMObject> list=new ArrayList<OOMObject>();
+        for (int i=0;i<=num;i++){
+            Thread.sleep(50);
+            list.add(new OOMObject());
+            System.gc();
+        }
+        return "hello";
+    }
+}
