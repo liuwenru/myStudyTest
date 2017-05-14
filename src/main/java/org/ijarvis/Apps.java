e4a3b622bc68893db128948a87b72a8760e40c34
ijarvis <ijarvis@sina.com>
2017-05-14 23:32:42 +0800

    add

--- src/main/java/org/ijarvis/Apps.java
+++ src/main/java/org/ijarvis/Apps.java
@@ -22,6 +22,7 @@ public class Apps {
     @RequestMapping("/home")
     public String hello(@RequestParam(value = "sec") String sec) throws IOException, InterruptedException {
         Thread.sleep(Long.parseLong(sec));
+        logger.info("vvvvv");
         return "hello";
     }
 }
