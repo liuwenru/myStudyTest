e62fcfb947602e00bd4b5048f11ea880574d0a78
ijarvis@sina.com <ijarvis@sina.com>
2018-08-29 23:05:11 +0800

    fix bug

--- src/main/java/com/epoint/SQLServerTest.java
+++ src/main/java/com/epoint/SQLServerTest.java
@@ -3,16 +3,20 @@ package com.epoint;
 import java.sql.*;
 
 public class SQLServerTest {
-    public static  void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static  void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = "jdbc:sqlserver://192.168.212.243\\SQL2008;databaseName=test";
-        String user = "sa";
-        String password = "Gepoint";
-        Connection con = DriverManager.getConnection(url, user, password);
-        System.out.println("连接成功！");
-        String sql = "select * from 2";
-        Statement com=con.createStatement();
-        com.executeQuery(sql);
-        System.out.println("11111");
+        while (true) {
+            String url = "jdbc:sqlserver://192.168.212.243\\SQL2008;databaseName=test";
+            String user = "sa";
+            String password = "Gepoint";
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("连接成功！");
+            String sql = "select  1";
+            Statement com = con.createStatement();
+            com.executeQuery(sql);
+            System.out.println("11111");
+
+            Thread.sleep(2000);
+        }
     }
 }
