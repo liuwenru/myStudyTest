ac2a7657cc7e2094271d6fe4ddab56fc3cc63d3e
liuwenru <ijarvis@sina.com>
2020-11-09 16:53:56 +0800

    fix bug

--- src/main/java/org/ijarvis/SpringMVCApp/Apps.java
+++ src/main/java/org/ijarvis/SpringMVCApp/Apps.java
@@ -4,7 +4,9 @@ package org.ijarvis.SpringMVCApp;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,17 +14,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @Controller
 public class Apps {
     private static ApplicationContext context ;
-    private static DruidDataSource dataSource ;
+    @Autowired
+    private DruidDataSource dataSource ;
     Logger logger = LoggerFactory.getLogger(Apps.class);
     @RequestMapping(value = "/loginTest", method = {RequestMethod.POST,RequestMethod.GET})
-    public String loginTest(HttpServletRequest request,HttpServletResponse response) throws InterruptedException {
+    public String loginTest(HttpServletRequest request,HttpServletResponse response) throws InterruptedException, SQLException {
         String account = request.getParameter("userName");
         String password = request.getParameter("password");
-        Thread.sleep(500);
+        Connection connection= dataSource.getConnection();
+        connection.createStatement().executeQuery("SELECT * from employees.employees");
         if (account.equals("admin")&&password.equals("1")){
             return "index";
         }else{
@@ -32,7 +38,6 @@ public class Apps {
     @RequestMapping(value = "/printmsg", method = {RequestMethod.POST,RequestMethod.GET})
     public String PrintMsg(HttpServletRequest request,HttpServletResponse response) throws InterruptedException {
         String msg = request.getParameter("msg");
-        Thread.sleep(500*1000);
         System.out.println(msg);
         return "index";
     }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+    <head>
+       <title>Index Page</title>
+    </head>
+    <body>
+        index.jsp
+    </body>
+</html>
