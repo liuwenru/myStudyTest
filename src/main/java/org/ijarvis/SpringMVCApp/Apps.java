9b224509895417a3d86d7579fe62d5ca03babbb5
liuwenru <ijarvis@sina.com>
2021-01-28 17:57:07 +0800

    fix bug

--- src/main/java/org/ijarvis/SpringMVCApp/Apps.java
+++ src/main/java/org/ijarvis/SpringMVCApp/Apps.java
@@ -1,6 +1,7 @@
 package org.ijarvis.SpringMVCApp;
 
 
+import com.alibaba.druid.pool.DataSourceClosedException;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,25 +16,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Controller
 public class Apps {
+    private static int a=2;
     private static ApplicationContext context ;
     @Autowired
     private DruidDataSource dataSource ;
     Logger logger = LoggerFactory.getLogger(Apps.class);
     @RequestMapping(value = "/loginTest", method = {RequestMethod.POST,RequestMethod.GET})
-    public String loginTest(HttpServletRequest request,HttpServletResponse response) throws InterruptedException, SQLException {
-        String account = request.getParameter("userName");
-        String password = request.getParameter("password");
+    public String loginTest(HttpServletRequest request,HttpServletResponse response) throws SQLException {
         Connection connection= dataSource.getConnection();
-        connection.createStatement().executeQuery("SELECT * from employees.employees");
-        if (account.equals("admin")&&password.equals("1")){
-            return "index";
-        }else{
-            return "login";
+        ResultSet resultSet= connection.createStatement().executeQuery("SELECT count(*) from employees.employees");
+
+        connection.close();
+        try {
+            if (a==2){
+                throw  new MyException("heigei");
+            }
+        }catch (Exception e){
+            System.out.println("heihei");
         }
+        return "index";
     }
     @RequestMapping(value = "/printmsg", method = {RequestMethod.POST,RequestMethod.GET})
     public String PrintMsg(HttpServletRequest request,HttpServletResponse response) throws InterruptedException {
@@ -50,4 +56,6 @@ public class Apps {
         Thread.sleep(Long.parseLong(time));
         return "hello";
     }
+
+
 }
---
+++ src/main/java/org/ijarvis/SpringMVCApp/MyException.java
@@ -0,0 +1,7 @@
+package org.ijarvis.SpringMVCApp;
+
+public class MyException extends Exception{
+    public MyException(String str){
+
+    }
+}
